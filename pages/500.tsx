767b92d6c612232ae043f3626aeea931e73719ba
MIROSLAVSH <sharkovskiymiroslav@gmail.com>
2023-03-23 20:29:28 +0300

    first commit

---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+	 <div>
+		<h1>Error 500</h1>
+		<p>looks like something went wrong. Please refresh the page or contact support.</p>
+	</div>
+  )
+}
+
+export default ErrorPage
